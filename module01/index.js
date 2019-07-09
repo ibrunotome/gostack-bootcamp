444b399079b18c1342cbe16c44d5763647a2e608
Bruno Tomé <ibrunotome@gmail.com>
2019-07-08 23:09:09 -0300

    Environment and concepts: CRUD

--- module01/index.js
+++ module01/index.js
@@ -2,16 +2,47 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Bruno Tomé" }
 
 const users = ['Diego', 'Robson', 'Maria']
 
+server.get('/users', (req, res) => {
+  return res.json(users)
+})
+
 server.get('/users/:index', (req, res) => {
   const { index } = req.params
 
   return res.json(users[index])
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body
+
+  users.push(name)
+
+  return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+
+  users[index] = name
+
+  return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params
+
+  users.splice(index, 1)
+
+  return res.status(204).send()
 })
 
 server.listen(3000)
