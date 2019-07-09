65fa05b0d0670d85a2c99c7a5bcc8ac41c8c5887
Bruno Tomé <ibrunotome@gmail.com>
2019-07-08 22:52:44 -0300

    Environments and concepts: Query & Route params

--- module01/index.js
+++ module01/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 
 const server = express()
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World' })
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Bruno Tomé" }
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  return res.json({ message: `Searching for ${id}` })
 })
 
 server.listen(3000)
