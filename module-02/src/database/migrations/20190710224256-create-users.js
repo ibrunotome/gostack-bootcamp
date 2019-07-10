7155a369a09e6eb6fac2380cf7fec22a8c91d4f3
Bruno Tomé <ibrunotome@gmail.com>
2019-07-10 19:55:49 -0300

    GoBarber: Add user migration

---
+++ module-02/src/database/migrations/20190710224256-create-users.js
@@ -0,0 +1,43 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users')
+  },
+}
