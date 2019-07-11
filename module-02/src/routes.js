20384ccdac2bbb02d28a1ef554bc5eff0332ced1
Bruno Tomé <ibrunotome@gmail.com>
2019-07-10 21:43:36 -0300

    GoBarber: JWT Auth

--- module-02/src/routes.js
+++ module-02/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import UserController from './app/controllers/UserController'
+import AuthController from './app/controllers/AuthController'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
+routes.post('/auth', AuthController.login)
 
 export default routes
