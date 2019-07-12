9af0d37220c7e202ce59580e3b731a2488943895
Bruno Tomé <ibrunotome@gmail.com>
2019-07-12 08:04:40 -0300

    GoBarber: Appointment cancellation

--- module-03/src/app/controllers/AppointmentController.js
+++ module-03/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup'
-import { startOfHour, parseISO, isBefore, format } from 'date-fns'
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
@@ -89,6 +89,28 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o dia ${formattedDate}`,
       user: provider_id,
     })
+
+    return res.json(appointment)
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id)
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(403).json({ error: 'Forbidden' })
+    }
+
+    const dateWithSub = subHours(appointment.date, 2)
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(403).json({
+        error: 'You can only cancel appointments 2 hours in advance',
+      })
+    }
+
+    appointment.canceled_at = new Date()
+
+    await appointment.save()
 
     return res.json(appointment)
   }
