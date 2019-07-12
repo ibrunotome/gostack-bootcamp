4ee3fec53de30baf3e8ef1780218cd981dca5400
Bruno Tomé <ibrunotome@gmail.com>
2019-07-11 22:31:10 -0300

    GoBarber: Listing schedule of provider

--- module-03/src/routes.js
+++ module-03/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController'
 
 import authMiddleware from './app/middlewares/auth'
 import AppointmentController from './app/controllers/AppointmentController'
+import ScheduleController from './app/controllers/ScheduleController'
 
 const routes = new Router()
 const upload = multer(multerConfig)
@@ -22,6 +23,8 @@ routes.put('/users', UserController.update)
 routes.get('/providers', ProviderController.index)
 routes.post('/appointments', AppointmentController.store)
 routes.get('/appointments', AppointmentController.index)
+
+routes.get('/schedules', ScheduleController.index)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
