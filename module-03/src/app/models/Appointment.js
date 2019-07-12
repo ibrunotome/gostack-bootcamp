0e8f2e0de1f722f058af6ae005bd02563d821fba
Bruno Tomé <ibrunotome@gmail.com>
2019-07-11 21:43:04 -0300

    GoBarber: Migration and model of appointment

---
+++ module-03/src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize'
+import Model from './Model'
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE_,
+      },
+      {
+        sequelize,
+      },
+    )
+
+    return this
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' })
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' })
+  }
+}
+
+export default Appointment
