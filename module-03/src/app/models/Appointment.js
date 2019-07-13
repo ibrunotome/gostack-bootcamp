40974b76479a789c1749467feded53637cbd04de
Bruno Tomé <ibrunotome@gmail.com>
2019-07-13 10:37:36 -0300

    GoBarber: Add virtual fields to appointments

--- module-03/src/app/models/Appointment.js
+++ module-03/src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize'
+import { isBefore, subHours } from 'date-fns'
 import Model from './Model'
 
 class Appointment extends Model {
@@ -7,6 +8,18 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date())
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2))
+          },
+        },
       },
       {
         sequelize,
