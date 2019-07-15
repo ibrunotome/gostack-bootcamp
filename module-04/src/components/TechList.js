536cea6e711475a41147aa7de0798328ac4c049b
Bruno Tomé <ibrunotome@gmail.com>
2019-07-15 20:07:21 -0300

    Introduction to React: State & Imutability

--- module-04/src/components/TechList.js
+++ module-04/src/components/TechList.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react'
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: ['Node.js', 'ReactJS', 'React Native'],
   }
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    })
+  }
+
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <li key={tech}>{tech}</li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </>
     )
   }
 }
