adfdbf78b8a9e2342d4274a57c427fd3f129a599
Bruno Tomé <ibrunotome@gmail.com>
2019-07-15 18:53:01 -0300

    Introduction to React: Importing css

--- module-04/webpack.config.js
+++ module-04/webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
 }
