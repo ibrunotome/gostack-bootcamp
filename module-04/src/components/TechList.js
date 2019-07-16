0f56f58f49f718d90ce6c6bdc742fb6d559512fb
Bruno Tomé <ibrunotome@gmail.com>
2019-07-16 18:34:15 -0300

    Introduction to React: Component lifecycle

--- module-04/src/components/TechList.js
+++ module-04/src/components/TechList.js
@@ -4,8 +4,29 @@ import TechItem from './TechItem'
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: ['Node.js', 'ReactJS', 'React Native'],
+    techs: [],
   }
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs')
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) })
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    // this.props, this.state
+
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value })
