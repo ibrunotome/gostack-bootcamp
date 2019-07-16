3efaaa65cce5e1ff5596899af674544e6ce4586c
Bruno Tomé <ibrunotome@gmail.com>
2019-07-16 18:25:29 -0300

    Introduction to React: Default Props & PropTypes

--- module-04/src/components/TechItem.js
+++ module-04/src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function TechItem({ tech, onDelete }) {
   return (
@@ -9,6 +10,15 @@ function TechItem({ tech, onDelete }) {
       </button>
     </li>
   )
+}
+
+TechItem.defaultProps = {
+  tech: 'Oculto',
+}
+
+TechItem.propTypes = {
+  tech: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
 }
 
 export default TechItem
