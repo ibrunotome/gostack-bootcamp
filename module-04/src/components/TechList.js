35317b81fd5e9cdde929d15c54db9ed72d0a28c2
Bruno Tomé <ibrunotome@gmail.com>
2019-07-16 18:10:00 -0300

    Introduction to React: Removing items of state

--- module-04/src/components/TechList.js
+++ module-04/src/components/TechList.js
@@ -19,13 +19,22 @@ class TechList extends Component {
     })
   }
 
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
       <>
         <form onSubmit={this.handleSubmit}>
           <ul>
             {this.state.techs.map(tech => (
-              <li key={tech}>{tech}</li>
+              <li key={tech}>
+                {tech}
+                <button onClick={() => this.handleDelete(tech)} type="button">
+                  Remover
+                </button>
+              </li>
             ))}
           </ul>
           <input
