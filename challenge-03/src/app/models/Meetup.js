c26d5789717ba7accb68af5b4b0fb32c1aebc0d2
Bruno Tomé <ibrunotome@gmail.com>
2019-07-20 12:17:21 -0300

    Meetapp: Add Meetup model/migration

---
+++ challenge-03/src/app/models/Meetup.js
@@ -0,0 +1,35 @@
+import { isBefore } from 'date-fns'
+import Sequelize from 'sequelize'
+import Model from './Model'
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date())
+          },
+        },
+      },
+      {
+        sequelize,
+      },
+    )
+
+    return this
+  }
+
+  static associate(models) {
+    this.hasMany(models.Subscription, { foreignKey: 'meetup_id' })
+    this.belongsTo(models.File, { foreignKey: 'file_id' })
+    this.belongsTo(models.User, { foreignKey: 'user_id' })
+  }
+}
+
+export default Meetup
