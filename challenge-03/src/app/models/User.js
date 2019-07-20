b911e818a0df5a98b3900b6586431486fdb93209
Bruno Tomé <ibrunotome@gmail.com>
2019-07-20 16:04:31 -0300

    Meetapp: Add Subscription model/migration

--- challenge-03/src/app/models/User.js
+++ challenge-03/src/app/models/User.js
@@ -27,6 +27,7 @@ class User extends Model {
 
   static associate(models) {
     this.hasMany(models.Meetup)
+    this.hasMany(models.Subscription)
   }
 
   checkPassword(password) {
--- challenge-03/src/database/index.js
+++ challenge-03/src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize'
 import User from '../app/models/User'
 import File from '../app/models/File'
-import databaseConfig from '../config/database'
 import Meetup from '../app/models/Meetup'
+import Subscription from '../app/models/Subscription'
+import databaseConfig from '../config/database'
 
-const models = [User, File, Meetup]
+const models = [User, File, Meetup, Subscription]
 
 class Database {
   constructor() {
