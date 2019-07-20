06318033b008de003ea3c8d3c8cd8340104a7c68
Bruno Tomé <ibrunotome@gmail.com>
2019-07-20 13:42:33 -0300

    Meetapp: User can edit his future meetups

--- challenge-03/src/app/controllers/MeetupController.js
+++ challenge-03/src/app/controllers/MeetupController.js
@@ -36,6 +36,41 @@ class MeetupController {
 
     return res.json(meetup)
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      file_id: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    })
+
+    try {
+      await schema.validate(req.body)
+    } catch (error) {
+      return res.status(422).json({ errors: error.errors })
+    }
+
+    const user_id = req.userId
+    const meetup = await Meetup.findByPk(req.params.id)
+
+    if (meetup.user_id !== user_id) {
+      return res.status(403).json({ error: 'Forbidden' })
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(422).json({ error: 'You cannot use a past date to create a new meetup' })
+    }
+
+    if (meetup.past) {
+      return res.status(422).json({ error: 'You cannot update a past meetup' })
+    }
+
+    await meetup.update(req.body)
+
+    return res.json(meetup)
+  }
 }
 
 export default new MeetupController()
