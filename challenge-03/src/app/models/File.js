00ae42e0d22936905a9fe23d90b8b436a46aba8e
Bruno Tomé <ibrunotome@gmail.com>
2019-07-20 11:50:45 -0300

    Meetapp: File management. Closes #34

---
+++ challenge-03/src/app/models/File.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize'
+import Model from './Model'
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      },
+    )
+
+    return this
+  }
+}
+
+export default File
--- challenge-03/src/routes.js
+++ challenge-03/src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
+
 import UserController from './app/controllers/UserController'
 import AuthController from './app/controllers/AuthController'
+import FileController from './app/controllers/FileController'
 import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/login', AuthController.login)
@@ -11,5 +16,6 @@ routes.post('/login', AuthController.login)
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
+routes.post('/files', upload.single('file'), FileController.store)
 
 export default routes
