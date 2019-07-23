f79a95103d97fd74c81bbf640c7a22e86a84a448
Bruno Tomé <ibrunotome@gmail.com>
2019-07-22 21:22:56 -0300

    Module 05: Using local storage

--- module-05/src/pages/Main/index.js
+++ module-05/src/pages/Main/index.js
@@ -14,6 +14,22 @@ export default class Main extends Component {
     loading: false,
   }
 
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories')
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) })
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories))
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value })
   }
