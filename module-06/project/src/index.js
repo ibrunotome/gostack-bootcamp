44ec0718b3ea5e07ddc4444e9a1abf51ad60d338
Bruno Tomé <ibrunotome@gmail.com>
2019-07-23 22:00:33 -0300

    Module 06: React Navigation

--- module-06/project/src/index.js
+++ module-06/project/src/index.js
@@ -1,28 +1,8 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
 
 import './config/ReactotronConfig'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-})
-
-console.tron.log('Hello World')
+import Routes from './routes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native</Text>
-    </View>
-  )
+  return <Routes />
 }
