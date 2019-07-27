02f71155288a356207fc3d31da34a7b6c42cb311
Bruno Tomé <ibrunotome@gmail.com>
2019-07-27 11:46:05 -0300

    Module 06: Loading and disabled

--- module-06/project/src/pages/Main/index.js
+++ module-06/project/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Keyboard } from 'react-native'
+import { Keyboard, ActivityIndicator } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '../../services/api'
@@ -22,11 +22,13 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   }
 
   handleAddUser = async () => {
     const { newUser, users } = this.state
 
+    this.setState({ loading: true })
     const response = await api.get(`/users/${newUser}`)
 
     const data = {
@@ -39,13 +41,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     })
 
     Keyboard.dismiss()
   }
 
   render() {
-    const { users, newUser } = this.state
+    const { users, newUser, loading } = this.state
 
     return (
       <Container>
@@ -59,8 +62,8 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? <ActivityIndicator color="#fff" /> : <Icon name="add" size={20} color="#FFF" />}
           </SubmitButton>
         </Form>
 
