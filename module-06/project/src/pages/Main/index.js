565bef22c6ac4d083753acfb287381bef415b59f
Bruno Tomé <ibrunotome@gmail.com>
2019-07-27 11:53:32 -0300

    Module 06: Saving into local storage

--- module-06/project/src/pages/Main/index.js
+++ module-06/project/src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Keyboard, ActivityIndicator } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '../../services/api'
@@ -23,6 +24,21 @@ export default class Main extends Component {
     newUser: '',
     users: [],
     loading: false,
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users')
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) })
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const { users } = this.state
+    if (prevState.users !== this.state.users) {
+      AsyncStorage.setItem('users', JSON.stringify(users))
+    }
   }
 
   handleAddUser = async () => {
