bdb3e91b47403731dfb2e5dc8d8d0f6700b3035b
Bruno Tomé <ibrunotome@gmail.com>
2019-07-28 12:26:34 -0300

    Challenge 04: Implement Header component

---
+++ challenge-04/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img
+          id="logo"
+          src="http://www.sclance.com/pngs/facebook-logo-white-png/facebook_logo_white_png_466671.png"
+        />
+        <div>
+          <span>Meu perfil</span>
+          <img src="https://i.kisscc0.com/20180815/vcq/kisscc0-facebook-computer-icons-user-profile-silhouette-bl-ninja-fb-profile-5b744db18bd8d9.9421314415343487215728.png" />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
