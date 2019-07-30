00d9819ec5a6a078497bbb545536b23daab11d62
Bruno Tomé <ibrunotome@gmail.com>
2019-07-29 21:25:25 -0300

    Module 07: Global Styles

--- module-07/.eslintrc.js
+++ module-07/.eslintrc.js
@@ -19,8 +19,8 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-uses-react': true,
-    'react/jsx-uses-vars': true,
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
