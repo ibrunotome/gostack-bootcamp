2be2b9a7c780bbcf2698295ca2907cee25021195
Bruno Tomé <ibrunotome@gmail.com>
2019-07-30 19:00:02 -0300

    Module 07: Adding to the cart

--- module-07/src/pages/Home/index.js
+++ module-07/src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { MdAddShoppingCart } from 'react-icons/md'
 import { formatPrice } from '../../util/format'
 import api from '../../services/api'
 
 import { ProductList } from './styles'
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   }
@@ -21,6 +22,15 @@ export default class Home extends Component {
     this.setState({ products: data })
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   render() {
     const { products } = this.state
     return (
@@ -28,10 +38,10 @@ export default class Home extends Component {
         {products.map(product => (
           <li key={product.id}>
             <img src={product.image} alt={product.title} />
-            <strong>Tenis massa com um titulo muito grande que quebra varias linhas</strong>
+            <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />3
               </div>
@@ -43,3 +53,5 @@ export default class Home extends Component {
     )
   }
 }
+
+export default connect()(Home)
