4eb3bbf8c9746cdd27aca4f95194527c7c93e645
Bruno Tomé <ibrunotome@gmail.com>
2019-07-30 18:38:52 -0300

    Module 07: Getting data from Api

--- module-07/src/pages/Home/index.js
+++ module-07/src/pages/Home/index.js
@@ -1,110 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
+import { formatPrice } from '../../util/format'
+import api from '../../services/api'
+
 import { ProductList } from './styles'
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tenis massa com um titulo muito grande que quebra varias linhas</strong>
-        <span>R$129,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products')
 
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$129,90</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    this.setState({ products: data })
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  render() {
+    const { products } = this.state
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>Tenis massa com um titulo muito grande que quebra varias linhas</strong>
+            <span>{product.priceFormatted}</span>
 
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91501831/tenis-nike-revolution-4-masculino-img.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
