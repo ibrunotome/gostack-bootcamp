ba7b6d7e14a8bc4877309077fd6539911f2eff9b
Bruno Tomé <ibrunotome@gmail.com>
2019-08-01 19:50:43 -0300

    Module 07: Showing cart quantity

--- module-07/src/pages/Home/index.js
+++ module-07/src/pages/Home/index.js
@@ -32,6 +32,8 @@ class Home extends Component {
 
   render() {
     const { products } = this.state
+    const { amount } = this.props
+
     return (
       <ProductList>
         {products.map(product => (
@@ -45,7 +47,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" />3
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -56,9 +59,17 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount
+
+    return amount
+  }, {}),
+})
+
 const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Home)
