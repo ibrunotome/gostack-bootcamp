f5505d3342ce95b890d053516a5d420b9c1332fb
Bruno Tomé <ibrunotome@gmail.com>
2019-08-01 19:43:24 -0300

    Module 07: Calculating total

--- module-07/src/pages/Cart/index.js
+++ module-07/src/pages/Cart/index.js
@@ -7,10 +7,11 @@ import {
   MdDelete,
 } from 'react-icons/md'
 
+import { formatPrice } from '../../util/format'
 import * as CartActions from '../../store/modules/cart/actions'
 import { Container, ProductTable, Total } from './styles'
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1)
   }
@@ -53,7 +54,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -73,7 +74,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -81,7 +82,16 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce(
+      (total, product) => total + product.price * product.amount,
+      0,
+    ),
+  ),
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
