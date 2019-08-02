eda33b0abaecbf2285946032f432a9c5c804cb52
Bruno Tomé <ibrunotome@gmail.com>
2019-08-02 19:18:04 -0300

    Module 08: Converting Rocketshoes to use hooks

---
+++ module-08/rocketshoes-hooks/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['standard', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
---
+++ module-08/rocketshoes-hooks/README.md
@@ -0,0 +1,5 @@
+# Module 07 - Flux Architecture
+
+<img width="100%" alt="Screenshot 2019-08-01 21 30 18" src="https://user-images.githubusercontent.com/4256471/62335802-ab698900-b4a3-11e9-8a83-bbbe87378ef6.png">
+
+<img width="100%" alt="Screenshot 2019-08-01 21 30 34" src="https://user-images.githubusercontent.com/4256471/62335801-aad0f280-b4a3-11e9-9134-8ecf00ffeccf.png">
