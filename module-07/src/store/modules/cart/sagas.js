5f8b1c6537390418a79c48b5645ab993b5f5cb73
Bruno Tomé <ibrunotome@gmail.com>
2019-08-01 21:06:21 -0300

    Module 07: Checking stock inside cart details

--- module-07/src/store/modules/cart/sagas.js
+++ module-07/src/store/modules/cart/sagas.js
@@ -2,7 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects'
 import { toast } from 'react-toastify'
 import api from '../../../services/api'
 import { formatPrice } from '../../../util/format'
-import { addToCartSuccess, updateAmount } from './actions'
+import { addToCartSuccess, updateAmountSuccess } from './actions'
 
 function* addToCart({ id }) {
   const productExists = yield select(state => state.cart.find(p => p.id === id))
@@ -20,7 +20,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount))
+    yield put(updateAmountSuccess(id, amount))
   } else {
     const response = yield call(api.get, `/products/${id}`)
 
@@ -33,4 +33,21 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)])
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return
+
+  const stock = yield call(api.get, `/stock/${id}`)
+  const stockAmount = stock.data.amount
+
+  if (amount > stockAmount) {
+    toast.error('Out of stock!')
+    return
+  }
+
+  yield put(updateAmountSuccess(id, amount))
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+])
