74ecacb35582960a914fe44793e523e8102b473c
Bruno Tomé <ibrunotome@gmail.com>
2019-08-03 11:00:11 -0300

    Challenge 06: Add ActivityIndicator for favorited repositories
    Closes #44

---
+++ challenge-06/project/src/pages/User/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import api from '../../services/api'
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+  ActivityIndicator,
+} from './styles'
+
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  })
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  }
+
+  state = {
+    stars: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props
+    const user = navigation.getParam('user')
+
+    const response = await api.get(`/users/${user.login}/starred`)
+
+    this.setState({ stars: response.data, loading: false })
+  }
+
+  render() {
+    const { navigation } = this.props
+    const { stars, loading } = this.state
+    const user = navigation.getParam('user')
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <ActivityIndicator size="large" color="#7159ca" animating={loading} />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    )
+  }
+}
