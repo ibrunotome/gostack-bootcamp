938ba7dfe862f2c99f544c48f4e6d39870fca07b
Bruno Tomé <ibrunotome@gmail.com>
2019-08-06 18:00:57 -0300

    Challenge 07: Start development of Home

--- challenge-07/challenge07/src/pages/Home/index.js
+++ challenge-07/challenge07/src/pages/Home/index.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { FlatList, Text } from 'react-native'
+import { Container } from './styles'
+
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
 
 export default class Home extends Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products')
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    this.setState({ products: data })
+  }
+
   render() {
-    return <Text>Home</Text>
+    const { products } = this.state
+
+    return (
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          extraData={this.props}
+          keyExtractor={item => String(item.id)}
+          renderItem={() => <Text>oi</Text>}
+        />
+      </Container>
+    )
   }
 }
