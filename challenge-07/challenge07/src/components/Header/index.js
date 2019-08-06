4f774fdf3dc0049ca8fee7ad69a2fe74e567bbe5
Bruno Tomé <ibrunotome@gmail.com>
2019-08-05 23:09:31 -0300

    Challenge 07: Add Basket Icon to Header

--- challenge-07/challenge07/src/components/Header/index.js
+++ challenge-07/challenge07/src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Container, Logo } from './styles'
+import { Container, Logo, BasketContainer, ItemCount } from './styles'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+Icon.loadFont()
 
 function Header() {
   return (
     <Container>
       <Logo />
+      <BasketContainer>
+        <Icon name="shopping-basket" color="#FFF" size={24} />
+        <ItemCount>0</ItemCount>
+      </BasketContainer>
     </Container>
   )
 }
--- challenge-07/challenge07/src/components/Header/styles.js
+++ challenge-07/challenge07/src/components/Header/styles.js
@@ -16,3 +16,25 @@ export const Logo = styled.Image.attrs({
   width: 185px;
   height: 24px;
 `
+
+export const BasketContainer = styled.TouchableOpacity`
+  height: 24px;
+  width: 24px;
+  flex: 1;
+  align-items: flex-end;
+  justify-content: flex-end;
+`
+export const ItemCount = styled.Text`
+  position: absolute;
+  text-align: center;
+  top: -8px;
+  right: -8px;
+  min-width: 18px;
+  min-height: 18px;
+  background: #7159ca;
+  color: #fff;
+  font-size: 12px;
+  padding: 2px;
+  border-radius: 9px;
+  overflow: hidden;
+`
