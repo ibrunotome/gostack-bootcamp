3c04d4e1100bac97533e757e19d0bb3e7a5cd0eb
Bruno Tomé <ibrunotome@gmail.com>
2019-08-06 21:08:53 -0300

    Challenge 07: Dynamically update badge in Header

--- challenge-07/challenge07/src/components/Header/index.js
+++ challenge-07/challenge07/src/components/Header/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Container, Logo, BasketContainer, ItemCount } from './styles'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 Icon.loadFont()
 
-function Header() {
+function Header({ amountInCart }) {
   return (
     <Container>
       <Logo />
       <BasketContainer>
         <Icon name="shopping-basket" color="#FFF" size={24} />
-        <ItemCount>0</ItemCount>
+        <ItemCount>{amountInCart || 0}</ItemCount>
       </BasketContainer>
     </Container>
   )
 }
 
-export default Header
+export default connect(
+  state => ({
+    amountInCart: state.cart.length,
+  }),
+  null,
+)(Header)
