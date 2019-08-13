9a236f8d7eeb176fd94086c57f76429045c65ed6
Bruno Tomé <ibrunotome@gmail.com>
2019-08-13 18:47:33 -0300

    Module 09: Adjustments in API

--- module-03/src/app/controllers/ScheduleController.js
+++ module-03/src/app/controllers/ScheduleController.js
@@ -22,6 +22,13 @@ class ScheduleController {
           [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     })
 
