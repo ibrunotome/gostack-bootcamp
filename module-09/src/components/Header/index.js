7bbb9f5d729544addc06332f06d7d3017d7c9601
Bruno Tomé <ibrunotome@gmail.com>
2019-08-14 18:52:49 -0300

    Module 09: Styling Notifications

--- module-09/src/components/Header/index.js
+++ module-09/src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+import Notifications from '~/components/Notifications'
 import logo from '~/assets/logo-purple.svg'
 
 import { Container, Content, Profile } from './styles'
@@ -14,6 +16,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Diego Fernandes</strong>
