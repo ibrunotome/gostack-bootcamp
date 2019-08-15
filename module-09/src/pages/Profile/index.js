5be772d0ad8dd5f8c1ec32355ab2f79cc9e73e2e
Bruno Tomé <ibrunotome@gmail.com>
2019-08-15 13:51:51 -0300

    Module 09: Implement logout

--- module-09/src/pages/Profile/index.js
+++ module-09/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Form, Input } from '@rocketseat/unform'
 import { Container } from './styles'
 
+import { signOut } from '~/store/modules/auth/actions'
 import { updateProfileRequest } from '~/store/modules/user/actions'
 
 import AvatarInput from './AvatarInput'
@@ -13,6 +14,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data))
+  }
+
+  function handleSignOut() {
+    dispatch(signOut())
   }
 
   return (
@@ -41,7 +46,9 @@ export default function Profile() {
         <button type="Submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   )
 }
--- module-09/src/store/modules/auth/sagas.js
+++ module-09/src/store/modules/auth/sagas.js
@@ -61,8 +61,14 @@ export function setToken({ payload }) {
   }
 }
 
+export function signOut() {
+  toast.success('Até mais')
+  history.push('/')
+}
+
 export default all([
   takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
 ])
