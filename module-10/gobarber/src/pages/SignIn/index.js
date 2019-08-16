d13b068c49996eac4a6f9ee15beb4b5b7609e81a
Bruno Tomé <ibrunotome@gmail.com>
2019-08-16 11:03:07 -0300

    Module 10: Accessibillity tips

--- module-10/gobarber/src/pages/SignIn/index.js
+++ module-10/gobarber/src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Image } from 'react-native'
 
 import logo from '~/assets/logo.png'
@@ -8,6 +8,10 @@ import Background from '~/components/Background'
 import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles'
 
 export default function SignIn({ navigation }) {
+  const passwordRef = useRef()
+
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -20,9 +24,18 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu email"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
 
-          <FormInput icon="lock-outline" secureTextEntry placeholder="Sua senha secreta" />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
 
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
         </Form>
--- module-10/gobarber/src/pages/SignUp/index.js
+++ module-10/gobarber/src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Image } from 'react-native'
 
 import logo from '~/assets/logo.png'
@@ -8,6 +8,11 @@ import Background from '~/components/Background'
 import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles'
 
 export default function SignUp({ navigation }) {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  function handleSubmit(params) {}
+
   return (
     <Background>
       <Container>
@@ -19,6 +24,8 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
           />
 
           <FormInput
@@ -27,15 +34,25 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
 
-          <FormInput icon="lock-outline" secureTextEntry placeholder="Sua senha secreta" />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
 
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
         </Form>
 
-        <SignLink onPress={() => navigation.navigate('SignUp')}>
-          <SignLinkText>Criar conta gratuita</SignLinkText>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já possuo uma conta</SignLinkText>
         </SignLink>
       </Container>
     </Background>
