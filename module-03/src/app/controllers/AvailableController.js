a1ff03cc7b155b31e490d2b082d5c1ce7e27c541
Bruno Tomé <ibrunotome@gmail.com>
2019-08-16 16:10:53 -0300

    Module 10: Confirm appointment

--- module-03/src/app/controllers/AvailableController.js
+++ module-03/src/app/controllers/AvailableController.js
@@ -43,7 +43,7 @@ class AvailableController {
 
       return {
         time,
-        value: format(value, "yyyy-MM-dd'T'HH::mm::ssxxx"),
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
         available: isAfter(value, new Date()) && !appointments.find(a => format(a.date, 'HH:mm') === time),
       }
     })
