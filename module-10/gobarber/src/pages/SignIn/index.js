8f48e81833715fb0ba53367fef0e86f15d15b7c9
Bruno Tomé <ibrunotome@gmail.com>
2019-08-16 10:00:52 -0300

    Module 10: Setting up background

--- module-10/gobarber/src/pages/SignIn/index.js
+++ module-10/gobarber/src/pages/SignIn/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native'
 
+import Background from '~/components/Background'
+
 export default function SignIn() {
-  return <Text>Oi</Text>
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  )
 }
--- module-10/gobarber/src/pages/SignUp/index.js
+++ module-10/gobarber/src/pages/SignUp/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-export default function SignIn() {
-  return <Text>SignUp</Text>
+import Background from '~/components/Background'
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  )
 }
