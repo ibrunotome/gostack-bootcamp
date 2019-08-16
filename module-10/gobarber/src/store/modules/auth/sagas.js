6743813b070dd6414077edfb7e208ba03b805226
Bruno Tomé <ibrunotome@gmail.com>
2019-08-16 11:20:51 -0300

    Module 10: Setting up Redux

---
+++ module-10/gobarber/src/store/modules/auth/sagas.js
@@ -0,0 +1,73 @@
+import { Alert } from 'react-native'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
+import api from '~/services/api'
+
+import { signInSuccess, signFailure } from './actions'
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload
+
+    const response = yield call(api.post, 'auth', {
+      email,
+      password,
+    })
+
+    const { token, user } = response.data
+
+    if (user.provider) {
+      Alert.alert('Erro no login', ['Aplicação somente para clientes'])
+      return
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`
+
+    yield put(signInSuccess(token, user))
+
+    // history.push('/dashboard')
+  } catch (error) {
+    Alert.alert('Falha na autenticação', 'Verifique seus dados')
+    yield put(signFailure())
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    })
+
+    // history.push('/')
+  } catch (error) {
+    Alert.alert('Falha no cadastro', 'Verifique seus dados')
+
+    yield put(signFailure())
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return
+
+  const { token } = payload.auth
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`
+  }
+}
+
+export function signOut() {
+  toast.success('Até mais')
+  // history.push('/')
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+])
