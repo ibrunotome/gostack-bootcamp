b61b3adf7a310f1c2c7bae0bf3ac525144b2ef06
Bruno Tomé <ibrunotome@gmail.com>
2019-08-16 10:32:05 -0300

    Module 10: Input & Button

--- module-10/gobarber/src/pages/SignIn/index.js
+++ module-10/gobarber/src/pages/SignIn/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { Text } from 'react-native'
 
 import Background from '~/components/Background'
+import Input from '~/components/Input'
+import Button from '~/components/Button'
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite seu nome" />
+
+      <Button>Entrar</Button>
     </Background>
   )
 }
