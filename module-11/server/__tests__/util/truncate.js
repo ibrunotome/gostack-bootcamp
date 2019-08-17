219a00d517ffde3da11db78a584b020c2cd3e888
Bruno Tomé <ibrunotome@gmail.com>
2019-08-17 20:48:20 -0300

    Module 11: Truncating database on each test

---
+++ module-11/server/__tests__/util/truncate.js
@@ -0,0 +1,9 @@
+import database from '../../src/database'
+
+export default function truncate() {
+  return Promise.all(
+    Object.keys(database.connection.models).map(key => {
+      return database.connection.models[key].destroy({ truncate: true, force: true })
+    }),
+  )
+}
