c4cb477455ec16dded18ded4dc94dc41bdae7c71
Bruno Tomé <ibrunotome@gmail.com>
2019-08-19 10:33:15 -0300

    Module 13: First test

---
+++ module-12/src/__tests__/components/TechList.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import TechList from '~/components/TechList'
+
+describe('TechList component', () => {
+  it('should be able to add new tech', () => {
+    const { getByText, getByTestId, debug } = render(<TechList />)
+
+    fireEvent.click(getByText('Adicionar'))
+
+    debug()
+
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'))
+  })
+})
--- module-12/src/index.js
+++ module-12/src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
