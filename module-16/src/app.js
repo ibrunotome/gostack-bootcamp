7058ccf79ff2b3447bfc0f99e29c9a4f0a7a1695
Bruno Tomé <ibrunotome@gmail.com>
2019-08-21 13:01:25 -0300

    Module 16: Using Helmet

--- module-16/src/app.js
+++ module-16/src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+import helmet from 'helmet'
 import Youch from 'Youch'
 import * as Sentry from '@sentry/node'
 import 'express-async-errors'
@@ -24,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler())
+    this.server.use(helmet())
     this.server.use(cors())
     this.server.use(express.json())
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
