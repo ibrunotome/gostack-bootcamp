10d688b24f05305836490410f8b9119e7c6d70b9
Bruno Tomé <ibrunotome@gmail.com>
2019-08-21 08:43:28 -0300

    Module 15: Setting up Code Push

---
+++ module-15/gobarber/src/pages/Dashboard/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
+import { withNavigationFocus } from 'react-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import api from '~/services/api'
+import Background from '~/components/Background'
+import Appointment from '~/components/Appointment'
+import { Container, Title, List } from './styles'
+
+function Dashboard({ isFocused }) {
+  const [appointments, setAppointments] = useState([])
+
+  async function loadAppointments() {
+    const response = await api.get('appointments')
+
+    setAppointments(response.data)
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments()
+    }
+  }, [isFocused])
+
+  async function handleCancel(id) {
+    try {
+      const response = await api.delete(`appointments/${id}`)
+
+      setAppointments(
+        appointments.map(appointment =>
+          appointment.id === id ? { ...appointment, canceled_at: response.data.canceled_at } : appointment,
+        ),
+      )
+    } catch (error) {
+      Alert.alert('Erro', 'Não conseguimos cancelar o agendamento')
+    }
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <Appointment onCancel={() => handleCancel(item.id)} data={item} />}
+        />
+      </Container>
+    </Background>
+  )
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => <Icon name="event" size={20} color={tintColor} />,
+}
+
+export default withNavigationFocus(Dashboard)
