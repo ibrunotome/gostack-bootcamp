31fd7190001de8e80495779b52e62257fc780eb7
Bruno Tomé <ibrunotome@gmail.com>
2019-08-24 14:36:50 -0300

    Challenge 09: Setting up routes

--- challenge-09/src/App.js
+++ challenge-09/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
+import Routes from './routes'
+import history from './services/history'
 
 function App () {
   return (
-    <h1>Hello World</h1>
+    <Router history={history}>
+      <Routes/>
+    </Router>
   )
 }
 
