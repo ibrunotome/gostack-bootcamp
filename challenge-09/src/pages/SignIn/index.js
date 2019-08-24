42ba58d373347ee4ecae669bf0369738d516a1d6
Bruno Tomé <ibrunotome@gmail.com>
2019-08-24 16:02:21 -0300

    Challenge 09: Stylizing auth pages

--- challenge-09/src/pages/SignIn/index.js
+++ challenge-09/src/pages/SignIn/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
+import logo from '~/assets/logo.svg'
 
 export default function SignIn () {
+  function handleSubmit (data) {
+    console.tron.log(data)
+  }
+
   return (
-    <h1>Login</h1>
+    <>
+      <img src={logo} alt="Meetapp" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Seu email" />
+        <Input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
   )
 }
--- challenge-09/src/pages/SignUp/index.js
+++ challenge-09/src/pages/SignUp/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom'
+import logo from '~/assets/logo.svg'
 
 export default function SignUp () {
   return (
-    <div />
+    <>
+      <img src={logo} alt="Meetapp" />
+
+      <form>
+        <input type="email" name="email" placeholder="Seu email" />
+        <input type="text" name="name" placeholder="Seu nome" />
+        <input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Entrar</button>
+        <Link to="/">Já possuo um login</Link>
+      </form>
+    </>
   )
 }
