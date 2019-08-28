f9a0207cf5eb7921a1ffd019817566b39278b6cb
Bruno Tomé <ibrunotome@gmail.com>
2019-08-27 23:40:21 -0300

    Challenge 10: Implement SignUp and SignIn screens

--- challenge-10/meetapp/src/pages/SignIn/index.js
+++ challenge-10/meetapp/src/pages/SignIn/index.js
@@ -1,12 +1,56 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { Image } from 'react-native'
+import logo from '~/assets/logo.png'
 
 import Background from '~/components/Background'
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles'
 
-export default function SignIn () {
+export default function SignIn ({ navigation }) {
+  const passwordRef = useRef()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit () {
+
+  }
+
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Seu email"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   )
 }
--- challenge-10/meetapp/src/pages/SignUp/index.js
+++ challenge-10/meetapp/src/pages/SignUp/index.js
@@ -1,12 +1,72 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { Image } from 'react-native'
+
+import logo from '~/assets/logo.png'
 
 import Background from '~/components/Background'
 
-export default function SignUp () {
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles'
+
+export default function SignUp ({ navigation }) {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit () {
+
+  }
+
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Seu nome"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Seu email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já possuo uma conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   )
 }
