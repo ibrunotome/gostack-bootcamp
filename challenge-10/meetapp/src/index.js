3f568e33995301de3759930944f0df90302812ce
Bruno Tomé <ibrunotome@gmail.com>
2019-08-28 07:38:39 -0300

    Challenge 10: Setting up StatusBar

--- challenge-10/meetapp/src/index.js
+++ challenge-10/meetapp/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { StatusBar } from 'react-native'
 import Routes from './routes'
 
 export default function App () {
   return (
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#25242F" />
     <Routes />
+    </>
   )
 }
